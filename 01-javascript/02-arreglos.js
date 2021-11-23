4772a1939c2ac8944816e087efa89737e123b950
Mahatma Quijano <mahatma.quijano@epn.edu.ec>
2021-11-23 09:00:04 -0500

    update/operadores

--- 01-javascript/02-arreglos.js
+++ 01-javascript/02-arreglos.js
@@ -24,4 +24,20 @@ for (let llave in objeto) {
     console.log("llave", llave);
 }
 
+// Agregar un elemento al final
+arreglo.push(11);   // Se pueden agregar varios elementos a la vez
 
+// Eliminar un elemento del final
+arreglo.pop();
+
+// Agregar un elemento al principio
+arreglo.unshift(5);     // Se pueden agregar varios elementos a la vez
+
+// Agregar en el indice 0
+arreglo.splice(0, 0, 4);
+
+// Eliminar elementos
+const indice = arreglo.indexOf(9);
+arreglo.splice(indice, 2);  // Elimina 2 elementos desde el 9
+
+console.log(arreglo)
---
+++ 01-javascript/03-operadores.js
@@ -0,0 +1,132 @@
+const arreglo = [
+    {
+        id: 1,
+        nombre: 'Adrian',
+        nota: 5
+    },
+    {
+        id: 2,
+        nombre: 'Vicente',
+        nota: 8
+    },
+    {
+        id: 3,
+        nombre: 'Carolina',
+        nota: 14
+    },
+    {
+        id: 4,
+        nombre: 'Wendy',
+        nota: 16
+    },
+    {
+        id: 5,
+        nombre: 'Andrea',
+        nota: 19
+    },
+    {
+        id: 6,
+        nombre: 'Pamela',
+        nota: 19
+    },
+    {
+        id: 7,
+        nombre: 'Cristian',
+        nota: 20
+    },
+    {
+        id: 8,
+        nombre: 'Daniel',
+        nota: 19
+    },
+    {
+        id: 9,
+        nombre: 'Lilly',
+        nota: 14
+    },
+    {
+        id: 10,
+        nombre: 'Ramiro',
+        nota: 12
+    }
+];
+
+// Find
+// Devuelve el primero que cumpla la condicion dada
+const respuestaFind = arreglo
+    .find(
+        function(valorActual, indiceActual, arregloCompleto) {
+            // console.log("Valor actual", valorActual);
+            // console.log("Indice actual", indiceActual);
+            // console.log("Arreglo completo", arregloCompleto);
+            return valorActual.nombre === "Cristian";    // Expresion
+        }
+    );
+
+console.log("Find:", respuestaFind);
+
+// Find Index
+// Devuelve el primero que cumpla la condicion dada
+const respuestaFindIndex = arreglo
+    .findIndex(
+        function(valorActual, indiceActual, arregloCompleto) {
+            return valorActual.nombre === "Cristian";
+        }
+    );
+// Si no encuentra, devuelve -1
+
+console.log("Find Index:", respuestaFindIndex)
+
+// ForEach
+const respuestaForEach = arreglo
+    .forEach(
+        function(valorActual, indiceActual, arregloCompleto) {
+            console.log("Valor actual", valorActual);
+        }
+    );
+
+console.log(respuestaForEach); // Undefined
+
+// Map
+const respuestaMap = arreglo
+    .map(
+        (valorActual, indiceActual, arregloCompleto) => {
+            const nuevoElemento = {
+                id: valorActual.id,
+                nombre: valorActual.nombre,
+                nota: valorActual.nota + 1
+            };
+            return nuevoElemento;
+        }
+    );
+console.log(respuestaMap);
+
+// Filer
+const respuestaFilter = arreglo
+    .filter(
+        (valorActual, indiceActual, arregloCompleto) => {
+            return valorActual.nota >= 14;
+        }
+    );
+
+console.log(respuestaFilter);
+
+// Some
+const respuestaSome = arreglo
+    .some(
+        (valorActual, indiceActual, arregloCompleto) => {
+            return valorActual.nota < 9;
+        }
+    );
+
+console.log("Some:", respuestaSome);
+
+// Every
+const respuestaEvery = arreglo
+    .every(
+        (valorActual, indiceActual, arregloCompleto) => {
+            return valorActual.nota > 14;
+        }
+    );
+
+console.log("Every:", respuestaEvery);
